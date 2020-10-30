884695232472d9ee4268ab0adda10542bfe2eb45
Antonio Mesquita <antonio@parafernalia.net.br>
2020-10-30 01:01:05 -0300

    test

--- src/main.java
+++ src/main.java
@@ -12,7 +12,6 @@ public class main {
 		// char piece = boardInstance.board[1][0];
 		play playInstance = play.getInstance();
 		// playInstance.selectField('p', "black", 1, 0, 2, 0)
-		// test
 		
 		System.out.printf("Move: %b\n",  playInstance.selectField('b', "white", 7, 2, 5, 0));
 		return;
